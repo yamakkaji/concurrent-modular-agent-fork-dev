65942bf101f3a5d98d859dbae0d74a56b7ebcfd5
maru-n <norihiro.maruyama@gmail.com>
2025-03-04 17:54:07 +0900

    change process mode to thread

--- concurrent_modular_agent/agent.py
+++ concurrent_modular_agent/agent.py
@@ -5,18 +5,22 @@ import concurrent_modular_agent as cma
 
 
 class Agent():
-    # def __init__(self, name:str, db_path: str = './db/default'):
-        # self.db_path = db_path
     def __init__(self, name:str):
         self.name = name
         self.modules = {}
-        self.messaging_clients = {}
+        self.main_process_modules = {}
         self.state = StateClient(self.name)
 
-    def add_module(self, module_name: str, module_function: callable):
-        self.modules[module_name] = module_function
-        
-    def _run_modules(self, agent_name, module_name, module_function, initialized_bariier):
+    def add_module(self, module_name: str, 
+                   module_function: callable,
+                   main_process: bool = False):
+        if main_process:
+            raise NotImplementedError("Main process modules are not yet implemented.")
+            # self.main_rocess_modules[module_name] = module_function
+        else:
+            self.modules[module_name] = module_function
+    
+    def _run_module_process(self, agent_name, module_name, module_function, initialized_bariier):
         state_client = cma.StateClient(agent_name, module_name)
         message_client = cma.MessageClient(agent_name, module_name)
         initialized_bariier.wait()
@@ -24,14 +28,25 @@ class Agent():
         
     def start(self, detach=True):
         module_processes = []
-        # initialized_bariier = threading.Barrier(len(self.modules))
-        initialized_bariier = multiprocessing.Barrier(len(self.modules))
+        
+        # run modules
+        initialized_bariier = threading.Barrier(len(self.modules))
+        # initialized_bariier = multiprocessing.Barrier(len(self.modules))
         for module_name, module_func in self.modules.items():
             # TODO: Use multiprocessing instead of threading, but it's sometime blocking bug with message passing.
-            process = multiprocessing.Process(target=self._run_modules, args=(self.name, module_name, module_func, initialized_bariier))
-            # process = threading.Thread(target=self._run_modules, args=(self.name, module_name, module_func, initialized_bariier))
+            process = threading.Thread(
+            # process = multiprocessing.Process(
+                target=self._run_module_process, 
+                args=(self.name, module_name, module_func, initialized_bariier))
+            
             module_processes.append(process)
             process.start()
+        # run main process modules
+        for modul_name, module_func in self.main_process_modules.items():
+            state_client = cma.StateClient(self.name, module_name)
+            message_client = cma.MessageClient(self.name, module_name)
+            module_func(state_client, message_client)
+            
         if not detach:
             for process in module_processes:
                 process.join()
