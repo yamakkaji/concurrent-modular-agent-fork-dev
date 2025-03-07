a2bf2c822b5149af6bff2a4ef0a65d5246e8c6b3
Norihiro MARUYAMA <norihiro.maruyama@gmail.com>
2025-03-07 18:37:44 +0900

    add chat example

--- examples/chat.py
+++ examples/chat.py
@@ -1,17 +1,41 @@
-import time
+from openai import OpenAI
 from concurrent_modular_agent import Agent, StateClient, MessageClient
 
 # This example is still a work in progress and not yet complete.
 
-def receiver(state:StateClient, message:MessageClient):
+def chat(state:StateClient, message:MessageClient):
+    openai_client = OpenAI()
     while True:
         m = message.receive()
-        print(f'receiver: received message: {m}')
+
+        messages = [
+            {"role": "developer", "content": "あなたは日本語で話すチャットボットです。"},
+        ]
+        for s in state.latest().texts[::-1]:
+            if s.startswith("user_message:"):
+                messages.append({"role": "user", "content": s[len("user_message:"):]})
+            elif s.startswith("assistant_message:"):
+                messages.append({"role": "assistant", "content": s[len("assistant_message:"):]})
+
+        completion = openai_client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+        )
+        output_message = completion.choices[0].message.content
+        print(f'ChatBot: {output_message}')
+        state.add(f'assistant_message:{output_message}')
+
+        message.send("main", 'finish')  # send signal to main thread for next message
     
-agent = Agent('myagent')
-agent.add_module("receiver", receiver)
+    
+agent = Agent('chat_agent')
+agent.state.clear()
+agent.add_module("chat", chat)
 agent.start()
+
+# this is main thread work as a module
 while True:
-    m = input("input message: ")
-    agent.message.send("receiver", m)
-    time.sleep(1)
+    m = input("User: ")
+    agent.state.add(f'user_message:{m}')
+    agent.message.send("chat", 'reply')  # send signal to chat module
+    agent.message.receive()  # wait for signal from chat module
