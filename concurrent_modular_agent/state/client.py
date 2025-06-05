1975b807bfe138b09442b01914b57e6e318a1400
maru-n <norihiro.maruyama@gmail.com>
2025-06-05 16:36:42 +0900

    refactering

--- concurrent_modular_agent/state/client.py
+++ concurrent_modular_agent/state/client.py
@@ -139,4 +139,23 @@ class StateClient():
     def retrieve(self, query_text:str, max_count:int=10, metadata:dict=None):
         warnings.warn("The 'retrieve' method is deprecated, use 'query' method instead.", DeprecationWarning)
         return self.query(query_text=query_text, max_count=max_count, metadata=metadata)
-    
+
+    @staticmethod
+    def get_all_names():
+        chromadb_client = chromadb.HttpClient(host='localhost', port=8000)
+        collections = chromadb_client.list_collections()
+        agent_memory_list = []
+        for collection in collections:
+            memory_name = StateClient._convert_collection_name_2_agent_name(collection.name)
+            agent_memory_list.append(memory_name)
+        return agent_memory_list
+
+    @staticmethod
+    def delete(name):
+        chromadb_client = chromadb.HttpClient(host='localhost', port=8000)
+        collection_name = StateClient._convert_agent_name_2_collection_name(name)
+        try:
+            chromadb_client.delete_collection(collection_name)
+        except chromadb.errors.NotFoundError:
+            raise ValueError(f"Agent memory with the name '{name}' does not exist.")
+        
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,14 @@
-from concurrent_modular_agent import utils, Agent
-from concurrent_modular_agent.state import State, StateClient
+from concurrent_modular_agent import Agent
+from concurrent_modular_agent.state import StateClient
 import pytest
+import tempfile
 
 def test_memory_list():
     agent = Agent('test_name1')
     state = StateClient("test_name2")
-    memory_list = utils.get_all_memory()
-    for m in memory_list:
-        print(m)
+    memory_list = StateClient.get_all_names()
+    # for m in memory_list:
+        # print(m)
     assert "test_name1" in memory_list, "test_name1 not found in memory list"
     assert "test_name2" in memory_list, "test_name2 not found in memory list"
     # for m in memory_list:
@@ -15,14 +16,22 @@ def test_memory_list():
         
 def test_memory_delete():
     state = StateClient("test_name3")
-    memory_list = utils.get_all_memory()
+    memory_list = StateClient.get_all_names()
     assert "test_name3" in memory_list, "test_name3 not found in memory list before deletion"    
     
     # Test deletion of an existing memory
-    utils.delete_memory("test_name3")
-    memory_list = utils.get_all_memory()
+    StateClient.delete("test_name3")
+    memory_list = StateClient.get_all_names()
     assert "test_name3" not in memory_list, "test_name3 found in memory list after deletion"
     
     # Test deletion of a non-existent memory
     with pytest.raises(ValueError):
-        utils.delete_memory("test_name3")
+        StateClient.delete("test_name3")
+        
+
+# def test_memory_backup():
+    # state = StateClient("test_name4")
+    # state.add("key1", "value1")
+    # state.add("key2", "value2")
+    # file_path = tempfile.NamedTemporaryFile(delete=False).name
+    # utils.backup_memory("test_name4", file_path)
